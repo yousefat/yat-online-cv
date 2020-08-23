c6bbcea011c0df045c6877ed82cc5d976afb346b
Younes <alturkeyy@gmail.com>
2020-08-24 00:50:02 +0300

    final configurations

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -33,6 +33,7 @@ const Header = () => {
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+        {cta && (
           <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
               <Link to="about" smooth duration={1000}>
@@ -40,6 +41,7 @@ const Header = () => {
               </Link>
             </span>
           </p>
+        )}
         </Fade>
       </Container>
     </section>
