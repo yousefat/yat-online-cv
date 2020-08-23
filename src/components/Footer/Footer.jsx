13a228557997c7223bfb16b6ccafa8e1ec7f5163
Younes <alturkeyy@gmail.com>
2020-08-23 22:45:16 +0300

    final edits

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -37,7 +37,7 @@ const Footer = () => {
             })}
         </div>
         <hr />
-        <p className="footer__text">© {new Date().getFullYear()} | Thank you for visiting </p>
+        <p className="footer__text">© {new Date().getFullYear()} | Thank you ❤!</p>
 
         {isEnabled && <GithubButtons />}
       </Container>
