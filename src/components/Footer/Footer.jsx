705a9d748bc42970431ec3325124823067e29c5b
yousefat <70079638+yousefat@users.noreply.github.com>
2020-08-24 18:53:55 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,14 +2,12 @@ import React, { useContext } from 'react';
 import { Container } from 'react-bootstrap';
 import { Link } from 'react-scroll';
 import PortfolioContext from '../../context/context';
-import GithubButtons from '../GithubButtons/GithubButtons';
 
 import { githubButtons } from '../../mock/data';
 
 const Footer = () => {
   const { footer } = useContext(PortfolioContext);
   const { networks } = footer;
-  const { isEnabled } = githubButtons;
 
   return (
     <footer className="footer navbar-static-bottom">
@@ -26,20 +24,18 @@ const Footer = () => {
               return (
                 <a
                   key={id}
-                  href={url || 'https://github.com/cobidev/gatsby-simplefolio'}
+                  href={url}
                   rel="noopener noreferrer"
                   target="_blank"
                   aria-label={name}
                 >
-                  <i className={`fa fa-${name || 'refresh'} fa-inverse`} />
+                  <i className={`fa fa-${name} fa-inverse`} />
                 </a>
               );
             })}
         </div>
         <hr />
         <p className="footer__text">© {new Date().getFullYear()} | Thank you.</p>
-
-        {isEnabled && <GithubButtons />}
       </Container>
     </footer>
   );
